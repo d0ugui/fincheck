2e51d46b959b10ee3fc82bb657544da62ca84939
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-04 10:13:16 -0300

    feat: create database module and set it global

--- api/src/modules/users/users.module.ts
+++ api/src/modules/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/shared/database/prisma.service';
-import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
