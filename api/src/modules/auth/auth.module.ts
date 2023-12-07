3accd06428ae5946e7a3b341dd703cb094a93b2f
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-07 09:14:23 -0300

    feat: create auth guard to validate requests headers

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [
     JwtModule.register({
+      global: true,
       signOptions: { expiresIn: '7d' },
       secret: env.jwtSecret,
     }),
--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  me(@Req() request: any) {
+    console.log({ meUserId: request.userId });
+
+    return this.usersService.getUserById('userId');
+  }
 }
