c00b54998a1ed286edb9f074605b8589c0ab6661
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-18 12:53:03 -0300

    refactor: remove unused code

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalGuards();
 
   await app.listen(3000);
 }
