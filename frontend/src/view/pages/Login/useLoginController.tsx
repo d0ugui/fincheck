920e77f3adee1999a4e1fa97f3c168b59ee573b6
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-22 14:19:28 -0300

    feat: install zod and create schema validation to login form

--- frontend/src/view/pages/Login/useLoginController.tsx
+++ frontend/src/view/pages/Login/useLoginController.tsx
@@ -1,10 +1,27 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, "E-mail é obrigatório")
+    .email("Informe um e-mail válido"),
+  password: z
+    .string()
+    .min(1, "Senha é obrigatória")
+    .min(8, "A senha deve conter pelo menos 8 dígitos"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function useLoginController() {
-  const { register, handleSubmit: hookFormHandleSubmit } = useForm();
+  const { register, handleSubmit: hookFormHandleSubmit } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   const handleSubmit = hookFormHandleSubmit((data) => {
-    console.log(data);
+    console.log("chama a api", data);
   });
 
   return { register, handleSubmit };
