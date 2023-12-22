53a543a78c35ed98e62e073ad359a4b598619730
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-22 11:50:52 -0300

    refactor: add ref to input element

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,30 +1,33 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
 interface InputProps extends ComponentProps<"input"> {
   name: string;
 }
 
-export function Input({ placeholder, name, id, ...props }: InputProps) {
-  const inputId = id ?? name;
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, name, id, ...props }, ref) => {
+    const inputId = id ?? name;
 
-  return (
-    <div className="relative">
-      <input
-        name={name}
-        id={inputId}
-        className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4
+    return (
+      <div className="relative">
+        <input
+          {...props}
+          ref={ref}
+          name={name}
+          id={inputId}
+          className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4
         peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none"
-        {...props}
-        placeholder=" "
-      />
+          placeholder=" "
+        />
 
-      <label
-        htmlFor={inputId}
-        className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700
+        <label
+          htmlFor={inputId}
+          className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700
         peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base transition-all"
-      >
-        {placeholder}
-      </label>
-    </div>
-  );
-}
+        >
+          {placeholder}
+        </label>
+      </div>
+    );
+  }
+);
