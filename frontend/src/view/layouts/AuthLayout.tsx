5ca7bfe5e42be78bcfca53fca77302812effda78
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-22 11:28:17 -0300

    fix: browser console warning and auth-layout margin

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -20,7 +20,7 @@ export function AuthLayout() {
           className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
         />
 
-        <div className="max-w-[656px] bg-white p-10 absolute bottom-8 rounded-b-[32px]">
+        <div className="max-w-[656px] bg-white p-10 absolute mx-8 bottom-8 rounded-b-[32px]">
           <Logo className="text-teal-900 h-8" />
 
           <p className="text-gray-700 font-medium text-xl mt-6">
