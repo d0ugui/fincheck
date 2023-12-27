9a4ef38b163655ae58b61d8f12dd28ea7ce320ff
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-27 19:29:35 -0300

    feat: get accessToken after user login/register and save token in localStorage to maintain session

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,14 +1,30 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useCallback, useState } from "react";
+import { localStorageKeys } from "../config/localStorageKeys";
 
 interface AuthContextValue {
   signedIn: boolean;
+  signin(accessToken: string): void;
 }
 
 export const AuthContext = createContext({} as AuthContextValue);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(
+      localStorageKeys.ACCESS_TOKEN
+    );
+
+    return !!storedAccessToken;
+  });
+
+  const signin = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+
+    setSignedIn(true);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signedIn: false }}>
+    <AuthContext.Provider value={{ signedIn, signin }}>
       {children}
     </AuthContext.Provider>
   );
