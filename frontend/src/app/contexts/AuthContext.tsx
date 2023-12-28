f616521febf396c127cf8bf75efa41b060837bb9
Douglas Oliveira <douglaspo_97@outlook.com>
2023-12-28 16:39:48 -0300

    feat: create signout method

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ import { localStorageKeys } from "../config/localStorageKeys";
 interface AuthContextValue {
   signedIn: boolean;
   signin(accessToken: string): void;
+  signout(): void;
 }
 
 export const AuthContext = createContext({} as AuthContextValue);
@@ -23,8 +24,14 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     setSignedIn(true);
   }, []);
 
+  const signout = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+
+    setSignedIn(false);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signedIn, signin }}>
+    <AuthContext.Provider value={{ signedIn, signin, signout }}>
       {children}
     </AuthContext.Provider>
   );
