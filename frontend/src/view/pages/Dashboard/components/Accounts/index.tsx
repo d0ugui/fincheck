f6f1066dff21638885daec8f5c4629ce79f43b1e
Douglas Oliveira <douglaspo_97@outlook.com>
2024-01-04 15:25:40 -0300

    feat: add accounts/transactions loading states

--- frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -2,6 +2,7 @@ import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { cn } from "../../../../../app/utils/cn";
 import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { Spinner } from "../../../../components/Spinner";
 import { EyeIcon } from "../../../../components/icons/EyeIcon";
 import { AccountCard } from "./AccountCard";
 import { SliderNavigation } from "./SliderNavigation";
@@ -14,87 +15,100 @@ export function Accounts() {
     windowWidth,
     areValuesVisible,
     toggleValueVisibility,
+    isLoading,
   } = useAccountsController();
 
   return (
     <div className="flex flex-col bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10">
-      <div>
-        <span className="text-white tracking-[-0.5px] block">Saldo total</span>
-
-        <div className="flex items-center gap-2">
-          <strong
-            className={cn(
-              "text-2xl tracking-[-1px] text-white",
-              !areValuesVisible && "blur-md"
-            )}
-          >
-            {formatCurrency(1000)}
-          </strong>
-
-          <button
-            className="w-8 h-8 flex items-center justify-center"
-            onClick={toggleValueVisibility}
-          >
-            <EyeIcon open={!areValuesVisible} />
-          </button>
+      {isLoading && (
+        <div className="w-full h-full flex items-center justify-center">
+          <Spinner className="text-teal-950/50 fill-white w-10 h-10" />
         </div>
-      </div>
+      )}
 
-      <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
-        <div>
-          <Swiper
-            spaceBetween={16}
-            slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
-            onSlideChange={(swiper) => {
-              setSliderState({
-                isBeginning: swiper.isBeginning,
-                isEnd: swiper.isEnd,
-              });
-            }}
-          >
-            <div
-              className="flex items-center justify-between mb-4"
-              slot="container-start"
-            >
-              <strong className="text-white tracking-[-1px] text-lg">
-                Minhas contas
+      {!isLoading && (
+        <>
+          <div>
+            <span className="text-white tracking-[-0.5px] block">
+              Saldo total
+            </span>
+
+            <div className="flex items-center gap-2">
+              <strong
+                className={cn(
+                  "text-2xl tracking-[-1px] text-white",
+                  !areValuesVisible && "blur-md"
+                )}
+              >
+                {formatCurrency(1000)}
               </strong>
 
-              <SliderNavigation
-                isBeginning={sliderState.isBeginning}
-                isEnd={sliderState.isEnd}
-              />
+              <button
+                className="w-8 h-8 flex items-center justify-center"
+                onClick={toggleValueVisibility}
+              >
+                <EyeIcon open={!areValuesVisible} />
+              </button>
             </div>
+          </div>
 
-            <SwiperSlide>
-              <AccountCard
-                color="#7950F2"
-                name="Nubank"
-                balance={1000.23}
-                type="CHECKING"
-              />
-            </SwiperSlide>
+          <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
+            <div>
+              <Swiper
+                spaceBetween={16}
+                slidesPerView={windowWidth >= 500 ? 2.1 : 1.2}
+                onSlideChange={(swiper) => {
+                  setSliderState({
+                    isBeginning: swiper.isBeginning,
+                    isEnd: swiper.isEnd,
+                  });
+                }}
+              >
+                <div
+                  className="flex items-center justify-between mb-4"
+                  slot="container-start"
+                >
+                  <strong className="text-white tracking-[-1px] text-lg">
+                    Minhas contas
+                  </strong>
 
-            <SwiperSlide>
-              <AccountCard
-                color="#E56700"
-                name="Inter"
-                balance={1000.23}
-                type="INVESTMENT"
-              />
-            </SwiperSlide>
+                  <SliderNavigation
+                    isBeginning={sliderState.isBeginning}
+                    isEnd={sliderState.isEnd}
+                  />
+                </div>
 
-            <SwiperSlide>
-              <AccountCard
-                color="#0f0"
-                name="Carteira"
-                balance={1000.23}
-                type="CASH"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
+                <SwiperSlide>
+                  <AccountCard
+                    color="#7950F2"
+                    name="Nubank"
+                    balance={1000.23}
+                    type="CHECKING"
+                  />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <AccountCard
+                    color="#E56700"
+                    name="Inter"
+                    balance={1000.23}
+                    type="INVESTMENT"
+                  />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <AccountCard
+                    color="#0f0"
+                    name="Carteira"
+                    balance={1000.23}
+                    type="CASH"
+                  />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- frontend/src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
+++ frontend/src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
@@ -6,12 +6,18 @@ export function useAccountsController() {
   const windowWidth = useWindowWidth();
   const { areValuesVisible, toggleValueVisibility } = useDashboard();
 
+
   const [sliderState, setSliderState] = useState({
     isBeginning: true,
     isEnd: false,
   });
 
   return {
-    sliderState, setSliderState, windowWidth, areValuesVisible, toggleValueVisibility
+    sliderState,
+    setSliderState,
+    windowWidth,
+    areValuesVisible,
+    toggleValueVisibility,
+    isLoading: false,
   }
 }
