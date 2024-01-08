62c94e5c88a86a2fba1172bf69824cce85ddf9e6
Douglas Oliveira <douglaspo_97@outlook.com>
2024-01-08 11:28:31 -0300

    fix: console warning, button inside a button

--- frontend/src/view/components/DropdownMenu.tsx
+++ frontend/src/view/components/DropdownMenu.tsx
@@ -8,7 +8,7 @@ function DropdownMenuRoot({ children }: { children: React.ReactNode }) {
 
 function DropdownMenuTrigger({ children }: { children: React.ReactNode }) {
   return (
-    <RdxDropdownMenu.Trigger className="outline-none">
+    <RdxDropdownMenu.Trigger className="outline-none" asChild>
       {children}
     </RdxDropdownMenu.Trigger>
   );
